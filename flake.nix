0858f0b61dcc56a6e0e466aa19e3488f2fe3e10b
rijkvp <rijk@rijkvp.nl>
2024-02-03 13:27:04 +0100

    initial

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell
+          rec {
+            nativeBuildInputs = [ rustc cargo clippy pkg-config ];
+            buildInputs = [
+              fontconfig
+              wayland
+              libxkbcommon
+              xorg.libX11
+              xorg.libXcursor
+            ];
+            LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+          };
+      });
+}

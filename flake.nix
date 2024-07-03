2667dc7b9ae0feb0572d85c0c44cfe9baa7f5035
Rijk van Putten <rijk@rijkvp.nl>
2024-07-03 14:22:36 +0200

    Add flake build using crane

--- flake.nix
+++ flake.nix
@@ -1,26 +1,54 @@
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    # Based on: https://crane.dev/examples/quick-start-simple.html
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, crane, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        craneLib = crane.mkLib pkgs;
+
+        commonArgs = rec {
+          src = craneLib.cleanCargoSource ./.;
+          strictDeps = true;
+
+          buildInputs = with pkgs; [
+            fontconfig
+            wayland
+            libxkbcommon
+            xorg.libX11
+            xorg.libXcursor
+          ];
+          nativeBuildInputs = with pkgs; [ clippy pkg-config ];
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+        };
+
+        launcher = craneLib.buildPackage (commonArgs // {
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        });
       in
       with pkgs;
       {
-        devShells.default = mkShell
-          rec {
-            nativeBuildInputs = [ rustc cargo rustfmt  clippy cargo-flamegraph pkg-config ];
-            buildInputs = [
-              fontconfig
-              wayland
-              libxkbcommon
-              xorg.libX11
-              xorg.libXcursor
-            ];
-            LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
-          };
+        checks = { inherit launcher; };
+
+        packages.default = launcher;
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = launcher;
+        };
+
+        devShells.default = craneLib.devShell {
+          checks = self.checks.${system};
+
+          packages = [
+            cargo-flamegraph
+          ];
+        };
       });
 }
